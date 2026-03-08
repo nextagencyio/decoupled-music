4c49bde880f3512d84c74cac6f101f4eea555217
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:46 -0500

    fix: mark header-dependent routes as dynamic

--- app/tour/page.tsx
+++ app/tour/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import TourDateCard from '../components/TourDateCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Tour Dates | Harmony Hall',
